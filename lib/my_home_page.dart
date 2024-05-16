fa268cf44511c667fb0096e6a29c9c75c1a61a92
Noppanut <Noptechnology@gmail.com>
2024-05-16 14:39:02 +0700

    AndroidApp-V2

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyHomePage  extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello World');
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text('My Home Page'),
+
+      ),
+    );
   }
 }
